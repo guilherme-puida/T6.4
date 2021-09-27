e0de9460028e650486943cfbf37088e1f26447b2
Guilherme Puida <guilhermepmoreira00@gmail.com>
2021-09-27 20:53:29 -0300

    Aba do cliente

--- src/view/ClientesTab.java
+++ src/view/ClientesTab.java
@@ -7,58 +7,19 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ClientesTab extends JPanel {
-    JList<Cliente> listaClientes;
-    JScrollPane listaClientesScroller;
+    ListaPanel listaClientes;
 
-    JPanel detailsPanel;
-
-    JTextField id, idade, nome, cpf, telefone, email, bairro, rua, numero, cep, categoria;
-    JButton submit;
+    ClienteDetails details;
 
     public ClientesTab() {
         setLayout(new BorderLayout(0, 0));
 
-        listaClientes = new JList<>(Loja.getInstance().getClientes());
-        listaClientes.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        listaClientes.setLayoutOrientation(JList.VERTICAL);
-        listaClientes.setVisibleRowCount(-1);
+        listaClientes = new ListaPanel(ListaPanel.CLIENTE);
+        add(listaClientes, BorderLayout.LINE_START);
 
-        listaClientesScroller = new JScrollPane(listaClientes);
-        listaClientesScroller.setPreferredSize(new Dimension(200, 800));
-        listaClientesScroller.setPreferredSize(new Dimension(200, 800));
-
-        add(listaClientesScroller, BorderLayout.LINE_START);
-
-        detailsPanel = new JPanel();
-        detailsPanel.setLayout(new GridLayout(3, 4, 50, 200));
-        id = new JTextField();
-        idade = new JTextField();
-        nome = new JTextField();
-        cpf = new JTextField();
-        telefone = new JTextField();
-        email = new JTextField();
-        bairro = new JTextField();
-        rua = new JTextField();
-        numero = new JTextField();
-        cep = new JTextField();
-        categoria = new JTextField();
-
-        detailsPanel.add(id);
-        detailsPanel.add(idade);
-        detailsPanel.add(nome);
-        detailsPanel.add(cpf);
-        detailsPanel.add(telefone);
-        detailsPanel.add(email);
-        detailsPanel.add(bairro);
-        detailsPanel.add(rua);
-        detailsPanel.add(numero);
-        detailsPanel.add(cep);
-        detailsPanel.add(categoria);
-
-        submit = new JButton("Submit");
-        detailsPanel.add(submit);
+        details = new ClienteDetails();
+        add(details, BorderLayout.CENTER);
 
 
-        add(detailsPanel, BorderLayout.CENTER);
     }
 }
