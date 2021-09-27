d19913f7d9b8c11452448b90067a932794ddc947
Guilherme Puida <guilhermepmoreira00@gmail.com>
2021-09-27 20:52:17 -0300

    Adicionando a toolbar principal

--- src/view/MainToolBar.java
+++ src/view/MainToolBar.java
@@ -3,24 +3,26 @@ package view;
 import javax.swing.*;
 
 public class MainToolBar extends JToolBar {
-    private final JButton adicionar, remover;
+    private final JButton adicionarCliente,
+            adicionarProduto,
+            adicionarFuncionario,
+            adicionarVenda, remover;
 
 
     public MainToolBar() {
-        adicionar = new JButton("Adicionar");
+        adicionarCliente = new JButton("Adicionar Cliente");
+        adicionarProduto = new JButton("Adicionar Produto");
+        adicionarFuncionario = new JButton("Adicionar Funcinario");
+        adicionarVenda = new JButton("Adicionar Venda");
+
         remover = new JButton("Remover");
 
-        add(adicionar);
+        add(adicionarCliente);
+        add(adicionarFuncionario);
+        add(adicionarProduto);
+        add(adicionarVenda);
         add(remover);
 
         setFloatable(false);
-    }
-
-    public JButton getAdicionar() {
-        return adicionar;
-    }
-
-    public JButton getRemover() {
-        return remover;
     }
 }
