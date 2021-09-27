8a2be89f1402dde8806c394da4c1f2bb7b3a7007
Guilherme Puida <guilhermepmoreira00@gmail.com>
2021-09-27 20:53:04 -0300

    Classe genérica para mostrar lista de objetos

--- src/view/ListaPanel.java
+++ src/view/ListaPanel.java
@@ -1,23 +1,40 @@
 package view;
 
-import model.Cliente;
-import model.Loja;
+import model.*;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class ListaPanel extends JPanel {
 
-    public ListaPanel() {
-        JList<Cliente> listaClientes = new JList<>(Loja.getInstance().getClientes());
-        listaClientes.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        listaClientes.setLayoutOrientation(JList.VERTICAL);
-        listaClientes.setVisibleRowCount(-1);
+    public static final int CLIENTE = 0;
+    public static final int FUNCIONARIO = 1;
+    public static final int CHOCOLATE = 2;
+    public static final int VENDA = 3;
 
-        JScrollPane listaClientesScroller = new JScrollPane(listaClientes);
-        listaClientesScroller.setPreferredSize(new Dimension(200, 800));
-        listaClientesScroller.setMinimumSize(new Dimension(200, 800));
+    private int selection;
 
-        add(listaClientesScroller);
+    private JList<?> lista;
+    private JScrollPane listaScroller;
+
+    public ListaPanel(int selection) {
+        this.selection = selection;
+        switch (selection) {
+            case 0 -> lista = new JList<>(Loja.getInstance().getClientes());
+            case 1 -> lista = new JList<>(Loja.getInstance().getFuncionarios());
+            case 2 -> lista = new JList<>(Loja.getInstance().getEstoque().getChocolates().toArray(new Chocolate[0]));
+            case 3 -> lista = new JList<>(Loja.getInstance().getVendas());
+        }
+
+
+        lista.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        lista.setLayoutOrientation(JList.VERTICAL);
+        lista.setVisibleRowCount(-1);
+
+        listaScroller = new JScrollPane(lista);
+        listaScroller.setPreferredSize(new Dimension(200, 800));
+        listaScroller.setMinimumSize(new Dimension(200, 800));
+
+        add(listaScroller);
     }
 }
