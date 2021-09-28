907aa7b9e96bde74a9ccb836e5e578ceb68f998e
Guilherme Puida <guilhermepmoreira00@gmail.com>
2021-09-28 20:41:21 -0300

    Main Toolbar Controller

---
+++ src/controller/MainToolBarController.java
@@ -0,0 +1,22 @@
+package controller;
+
+
+import model.Cliente;
+import model.Loja;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainToolBarController implements ActionListener {
+    Loja loja = Loja.getInstance();
+
+    public MainToolBarController(JList jList) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
