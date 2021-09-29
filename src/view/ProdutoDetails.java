474e6af0c7b8edec496552388b5fdf05f2d6a03d
guilherme.moreira <guilhermepmoreira00@gmail.com>
2021-09-29 13:45:09 -0300

    Começando aba de produtos

--- src/view/ProdutoDetails.java
+++ src/view/ProdutoDetails.java
@@ -12,6 +12,9 @@ public class ProdutoDetails extends DetailsPanel{
     porcentagemCacau;
     private final JCheckBox contemGluten, contemLactose, artesanal;
 
+    private final JTextField sabor, nibs, recheio, frutas;
+    private final JCheckBox alcoolico;
+
     public ProdutoDetails(int escolha) {
         super("Detalhes do produto");
 
@@ -26,14 +29,67 @@ public class ProdutoDetails extends DetailsPanel{
         precoVenda = new JTextField();
         porcentagemCacau = new JTextField();
 
+        sabor = new JTextField();
+        nibs = new JTextField();
+        recheio = new JTextField();
+        frutas = new JTextField();
+
         contemGluten = new JCheckBox();
         contemLactose = new JCheckBox();
         artesanal = new JCheckBox();
 
+        alcoolico = new JCheckBox();
+
         GridBagConstraints left = getLeft();
         GridBagConstraints right = getRight();
 
-        add(new JLabel("Id:"), left);
+        getInnerDetailsPanel().add(new JLabel("Id:"), left);
+        getInnerDetailsPanel().add(id, right);
+        getInnerDetailsPanel().add(new JLabel("Peso:"), left);
+        getInnerDetailsPanel().add(peso, right);
+        getInnerDetailsPanel().add(new JLabel("Nome:"), left);
+        getInnerDetailsPanel().add(nome, right);
+        getInnerDetailsPanel().add(new JLabel("Descrição:"), left);
+        getInnerDetailsPanel().add(descricao, right);
+        getInnerDetailsPanel().add(new JLabel("Tipo:"), left);
+        getInnerDetailsPanel().add(tipo, right);
+        getInnerDetailsPanel().add(new JLabel("Preço de Compra:"), left);
+        getInnerDetailsPanel().add(precoCompra, right);
+        getInnerDetailsPanel().add(new JLabel("Preço de Venda:"), left);
+        getInnerDetailsPanel().add(precoVenda, right);
+        getInnerDetailsPanel().add(new JLabel("Porcentagem de Cacau:"), left);
+        getInnerDetailsPanel().add(porcentagemCacau, right);
+
+        switch(escolha) {
+            case BARRA -> {
+                getInnerDetailsPanel().add(new JLabel("Sabor:"), left);
+                getInnerDetailsPanel().add(sabor, right);
+                getInnerDetailsPanel().add(new JLabel("Nibs:"), left);
+                getInnerDetailsPanel().add(nibs, right);
+            }
+            case CHOCOTONE -> {
+                getInnerDetailsPanel().add(new JLabel("Recheio"), left);
+                getInnerDetailsPanel().add(recheio, right);
+                getInnerDetailsPanel().add(new JLabel("Frutas:"), left);
+                getInnerDetailsPanel().add(frutas, right);
+            }
+            case TRUFA -> {
+                getInnerDetailsPanel().add(new JLabel("Recheio"), left);
+                getInnerDetailsPanel().add(recheio, right);
+                getInnerDetailsPanel().add(new JLabel("Alcoólico:"), left);
+                getInnerDetailsPanel().add(alcoolico, right);
+            }
+        }
+
+
+        left.gridx = GridBagConstraints.RELATIVE;
+        right.gridx = GridBagConstraints.RELATIVE;
+        getInnerDetailsPanel().add(new JLabel("Contém Gluten:"), left);
+        getInnerDetailsPanel().add(contemGluten, right);
+        getInnerDetailsPanel().add(new JLabel("Contém Lactose:"), left);
+        getInnerDetailsPanel().add(contemLactose, right);
+        getInnerDetailsPanel().add(new JLabel("Artesanal:"), left);
+        getInnerDetailsPanel().add(artesanal, right);
 
     }
 }
--- src/view/ProdutosTab.java
+++ src/view/ProdutosTab.java
@@ -1,7 +1,21 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ProdutosTab extends JPanel {
+        private final ListaPanel listaProdutos;
 
+        private final ProdutoDetails details;
+
+        public ProdutosTab() {
+            setLayout(new BorderLayout());
+
+            listaProdutos = new ListaPanel(ListaPanel.CHOCOLATE);
+            add(listaProdutos, BorderLayout.LINE_START);
+
+            details = new ProdutoDetails(ProdutoDetails.TRUFA);
+            add(details, BorderLayout.CENTER);
+
+        }
 }
